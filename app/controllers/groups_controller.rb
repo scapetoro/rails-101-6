a7ebfd0e832d7ea3eb96b87f927fc5bb024608fb
lihantao <htl00001111@163.com>
2017-03-26 18:08:44 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,13 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     @group.update(group_params)
-    redirect_to group_path, notice: "Update Success"
+    redirect_to groups_path, notice: "Update Success"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "Movie deleted"
   end
 
   private
