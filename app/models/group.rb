3eed21830d35603d72740f8c27d0dcd142479479
lihantao <htl00001111@163.com>
2017-03-27 20:23:36 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,7 @@ class Group < ApplicationRecord
   has_many :reviews
   validates :movie, presence: true
   validates :description, presence: true
+
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
