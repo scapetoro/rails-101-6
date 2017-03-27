6919c6e8fc4f196854110d75f7a2a29767455567
lihantao <htl00001111@163.com>
2017-03-27 21:13:44 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -45,6 +45,31 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Movie deleted"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本电影群成功！"
+    else
+      flash[:warning] = "你已经是本群成员！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本电影群！"
+    else
+      flash[:warning] = "你不是本群成员，怎么退出？"
+    end
+    redirect_to group_path(@group)
+  end
+
   private
 
   def find_group_and_check_permission
